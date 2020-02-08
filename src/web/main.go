3e4da8a52ff1b3cb5d6747f1ad68af2acee5419f
DIO0550 <diot12345@yahoo.co.jp>
2020-02-09 00:28:32 +0900

    indexテンプレートにDBで受け取ったデータを渡すように修正。

--- src/web/main.go
+++ src/web/main.go
@@ -4,26 +4,42 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"github.com/web/package"
 )
 
 var templates = make(map[string]*template.Template)
 
+type TemplateData struct {
+	Title string
+	Data interface{}
+}
+
 func main() {
 	port := "8080"
-	templates["index"] = loadTemplate("index")
-	http.HandleFunc("/", handleIndex)
+	templates["index"] = LoadTemplate("index")
+	http.Handle("/stylesheet/", http.StripPrefix("/stylesheet/", http.FileServer(http.Dir("stylesheet/")))) 
+	http.HandleFunc("/", HandleIndex)
 	log.Printf("Server listening on port %s", port)
 	log.Print(http.ListenAndServe(":"+port, nil))
 }
 
-func handleIndex(w http.ResponseWriter, r *http.Request) {
-	if err := templates["index"].Execute(w, nil)
+// indexのハンドルを返す
+func HandleIndex(w http.ResponseWriter, r *http.Request) {
+	rooms, err := managementDB.FetchAllConferenceRoomData()
+	if err != nil {
+		log.Printf(err.Error())
+	}
+
+	data := TemplateData{Title: "index", Data: rooms}
+
+	if err = templates["index"].Execute(w, data)
 	 err != nil {
 		log.Printf("failed to execute template: %v", err)
 	}
 }
 
-func loadTemplate(templateName string) *template.Template {
+// テンプレートをロードする
+func LoadTemplate(templateName string) *template.Template {
 	t, err := template.ParseFiles("template/"+templateName+".html", "template/_header.html", "template/_footer.html")
 	if err != nil {
 		log.Fatalf("template error: %v", err)
