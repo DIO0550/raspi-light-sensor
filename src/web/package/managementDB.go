183998c67c1eee2c1e793ad3184105ef287b0cc9
DIO0550 <diot12345@yahoo.co.jp>
2020-02-09 00:26:14 +0900

    database.go削除。DB操作系の処理を担当する自作パッケージ追加。

---
+++ src/web/package/managementDB.go
@@ -0,0 +1,54 @@
+package  managementDB
+
+import (
+	"database/sql"
+    "fmt"
+    "log"
+
+    _ "github.com/go-sql-driver/mysql"
+)
+
+type ConferenceRoom struct {
+    Id int
+    Name string
+    UsageSituation bool
+}
+
+func ConnectionDB() (*sql.DB, error) {
+    db, err := sql.Open("mysql", "golang:golang@tcp(192.168.0.2:3306)/golang?parseTime=true")
+    if err != nil {
+        log.Printf(err.Error())
+        fmt.Println(err)
+    }
+    return db, err
+}
+
+func FetchAllConferenceRoomData()(conferenceRooms []ConferenceRoom, err error) {
+    db, err := ConnectionDB()
+    if err != nil {
+        log.Printf(err.Error())
+        fmt.Println(err)
+        return nil, err;
+    } 
+    defer db.Close()
+
+    rows, err := db.Query("SELECT * FROM conference_room ORDER BY NAME ASC")
+    if err != nil {
+        log.Printf(err.Error())
+        fmt.Println(err)
+        return nil, err;
+    }
+
+    for rows.Next() {
+        r := ConferenceRoom{}
+        err = rows.Scan(&r.Id, &r.Name, &r.UsageSituation)
+        if err != nil {
+            log.Printf(err.Error())
+            fmt.Println(err)
+            return nil, err;
+        }
+        conferenceRooms = append(conferenceRooms, r)
+    }
+    
+    return
+}
