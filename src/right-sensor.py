7fb303de03029d6087ff754c7dc86fe5210abd2e
DIO0550 <diot12345@yahoo.co.jp>
2020-02-14 23:49:35 +0900

    サーバ側のAPIを呼び出す処理を追加。

--- src/right-sensor.py
+++ src/right-sensor.py
@@ -1,8 +1,30 @@
 from gpiozero import MCP3008
 from time import sleep
+import urllib.request
+import json
+
+Bright = 60.0
+ConferenceRoomUpdateAPI = "http://localhost:8080/conferenceroom/update"
 adc = MCP3008(channel=0, device=0)
 
 while True:
     brightness = (adc.value * 3.3) * 100
-    # TODO: 明るいかくらいかの判別
+    usage_situation: bool = brightness > Bright
+    param = {"name": "7階D", "usage_situation": usage_situation}
+    method = "POST"
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    json_data = json.dumps(param).encode('utf-8')
+    print(json_data) 
+    request = urllib.request.Request(ConferenceRoomUpdateAPI, data=json_data, headers=headers, method=method)
+    try:
+        with urllib.request.urlopen(request) as response:
+            body = json.load(response)
+            print(body)
+    except urllib.error.HTTPError as err:
+        print(err.code)
+    except urllib.error.URLError as err:
+        print(err.reason)
+
     sleep(5)
